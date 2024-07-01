4911df1ce01790b67107efc4889d2c9d1897182e
vjpelissari <vinicius.pelissari@gmail.com>
2024-06-30 22:25:37 -0300

    Traversing the DOM

--- traversi/dom/shopping-list-start/script.js
+++ traversi/dom/shopping-list-start/script.js
@@ -37,9 +37,10 @@ firstItem.style.color = 'blue';
 */
 
 // querySelectorAll
-const listItems = document.querySelectorAll('.item');
-console.log(listItems[0].innerText);
+//const listItems = document.querySelectorAll('.item');
+//console.log(listItems[0].innerText);
 
+/*
 listItems[1].style.color = 'red';
 
 listItems.forEach((item, index) => {
@@ -50,7 +51,81 @@ listItems.forEach((item, index) => {
     }
 
     if (index === 0) {
-        item.innerText = 'Orangeass';
+        // item.innerText = 'Orangeass';
+        item.innnerHTML = ` Oranges
+        <button class="remove-item btn-link text-red">
+            <i class="fa-solid fa-xmark"></i>
+        </button>`;
     }
-
 });
+*/
+
+/*
+const listItems2 = document.getElementsByClassName('item');
+console.log(listItems2[2].innerText);
+
+// if we have an html colletion, and want to go thought it,
+// first we have to convert in an array.
+const listItemsArray = Array.from(listItems2);
+listItemsArray.forEach((item) => {
+    console.log(item.innerText);
+})
+    ;
+*/
+
+/*
+// GETTING CHILD FROM PARENT
+const parent = documento.querySelector('.parent');
+output = parent.children;
+output = parent.children[1].innerText;
+parent.children[1].innerText = 'Child Two';
+parent.children[1].style.color = 'red';
+parent.firstElementChild.innerText = 'Child One';
+parent.lastElementChild.innerText = 'Child One';
+
+// GETTING PARENT ELEMENT FROM A CHILD
+const child = document.querySelector('.child');
+output = child.parentElement;
+child.parentElement.style.border = '1px solid #ccc';
+
+// GETTING SIBLING ELEMENTS
+const secondItem = document.querySelector('.child:nth-child(2)');
+secondItem.nextElementSibling.style.color = 'green';
+secondItem.previousElementSibling.style.color = 'green';
+*/
+
+// CREATE ELEMENTS IN DOM
+// creating a div
+const div = document.createElement('div');
+div.className = 'myelement';
+div.id = 'my-element';
+div.setAttribute('title', 'My Element');
+
+// that way isnt good for creating text.
+div.innerText = 'Text in div';
+// then, create like this:
+const text = document.createTextNode('Hello');
+div.appendChild(text);
+
+// putting a element into the DOM:
+document.body.appendChild(div);
+// or:
+document.querySelector('ul').appendChild(div);
+
+// Now, let´s creating elements in DOM.
+// Firts, in a Quick an Dirty way.
+// After, in a Clean & Performant
+
+// Quick & Dirt
+function createListItem(item) {
+    const li = document.createElement('li');
+
+    li.innerHTML = `${item}
+    <button class="remove-item btn-link text-red">
+      <i class="fa-solid fa-xmark"></i>
+    </button>`;
+
+    document.querySelector('.items').appendChild(li);
+}
+
+// Clean & Performant
